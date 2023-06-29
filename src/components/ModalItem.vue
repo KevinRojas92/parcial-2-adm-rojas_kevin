1fb4db29c05a10c0b1b90a9dc1c2e3ed97b84d8a
Kevin Rojas <kevinrojasdg@gmail.com>
2023-06-28 22:36:54 -0300

    Create component modal

---
+++ src/components/ModalItem.vue
@@ -0,0 +1,34 @@
+<template>
+
+<div>
+    <div>
+        <div>
+            <h3>Agrega una factura</h3>
+            <a href="">x</a>
+        </div>
+    
+        <form action="">
+            <div>
+                <label for="motivo">Motivo</label>
+                <input id="motivo" type="text" name="motivo" placeholder="Motivo">
+            </div>
+        
+            <div style="margin-top: 25px;">
+                <label for="valor">Valor</label>
+                <input id="valor" type="number" name="valor" placeholder="Valor">
+            </div>
+        </form>
+    
+        <button>Agregar</button>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'ModalItem',
+  props: {
+  }
+}
+</script>
