00c5b1527550ccc09c9d251fe51c616e032b9360
Kevin Rojas <kevinrojasdg@gmail.com>
2023-06-28 22:36:39 -0300

    Create component resultados

---
+++ src/components/ResultadosItems.vue
@@ -0,0 +1,43 @@
+<template>
+
+<div>
+    <div>
+        <!--agregar-nueva-factura @mostrar-modal="mostrarModal"></agregar-nueva-factura-->
+    
+        <div>
+            <h2>Mis facturas:</h2>
+
+            <div>
+                <div>
+                    <h3>sgsdgsdgs</h3>
+                    <button>x</button>
+                </div>
+    
+                <div style="margin-top: 25px;">
+                    <p>usuario1</p>
+                    <p>precioUsuario1</p>
+                </div>
+    
+                <div style="margin-top: 20px;">
+                    <p>$usuario2</p>
+                    <p>$precioUsuario2</p>
+                </div>
+    
+                <div>
+                    <p>Total</p>
+                    <p>precio</p>
+                </div>
+            </div>
+        </div>
+    </div>
+ </div>
+    
+</template>
+    
+<script>
+export default {
+    name: 'ResultadosItems',
+    props: {
+    }
+}
+</script>
