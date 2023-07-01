77bda1fc7b9206393bf42b71cd2b032fe0d732fa
Kevin Rojas <kevinrojasdg@gmail.com>
2023-06-30 21:06:47 -0300

    update

--- src/components/ResultadosItems.vue
+++ src/components/ResultadosItems.vue
@@ -5,25 +5,27 @@
         <div>
             <h2>Mis facturas:</h2>
 
-            <div>
+            <p style="margin-top: 20px;" v-if="!facturasStorage">Aún no has agregado facturas.</p>
+
+            <div v-for="i in facturasStorage">
                 <div>
-                    <h3>sgsdgsdgs</h3>
+                    <h3>{{i.razon}}</h3>
                     <button>x</button>
                 </div>
     
                 <div style="margin-top: 25px;">
-                    <p>usuario1</p>
-                    <p>precioUsuario1</p>
+                    <p>Usuario 1</p>
+                    <p>${{i.precioUsuario1}}</p>
                 </div>
     
                 <div style="margin-top: 20px;">
-                    <p>$usuario2</p>
-                    <p>$precioUsuario2</p>
+                    <p>Usuario 2</p>
+                    <p>${{i.precioUsuario2}}</p>
                 </div>
     
                 <div>
                     <p>Total</p>
-                    <p>precio</p>
+                    <p>{{i.precio}}</p>
                 </div>
             </div>
         </div>
@@ -35,7 +37,15 @@
 <script>
 export default {
     name: 'ResultadosItems',
+
     props: {
-    }
+        
+    },
+
+    data: function () {
+        return {
+            facturasStorage: JSON.parse(localStorage.getItem("facturas"))
+        }
+    },
 }
 </script>
