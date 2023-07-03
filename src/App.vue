130b8d92f737c92b4b90b0e6952e6bf4edbcf1e5
Kevin Rojas <kevinrojasdg@gmail.com>
2023-07-03 12:20:37 -0300

    Add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,40 @@
 <template>
 
-<div id="app">
-  <SueldoUsuarios/>
-
-  <ModalItem/>
-</div>
-
-</template>
-
-<script>
-
-import SueldoUsuarios from './components/SueldoUsuarios.vue';
-import ModalItem from './components/ModalItem.vue';
-
-export default {
-  name: 'App',
-
-  data: function () {
-    return {
-      sueldoU1: "",
-      sueldoU2: "",
-
-      modal: false,
-
-      motivo: "",
-      valor: "",
-
-      factura: {},
-
-      allFacturas: []
+  <div id="app">
+    <SueldoUsuarios/>
+  
+    <ModalItem/>
+  </div>
+  
+  </template>
+  
+  <script>
+  
+  import SueldoUsuarios from './components/SueldoUsuarios.vue';
+  import ModalItem from './components/ModalItem.vue';
+  
+  export default {
+    name: 'App',
+  
+    data: function () {
+      return {
+        sueldoU1: "",
+        sueldoU2: "",
+  
+        modal: false,
+  
+        motivo: "",
+        valor: "",
+  
+        factura: {},
+  
+        allFacturas: []
+      }
+    },
+  
+    components: {
+      SueldoUsuarios,
+      ModalItem,
     }
-  },
-
-  components: {
-    SueldoUsuarios,
-    ModalItem,
   }
-}
-</script>
+  </script>
